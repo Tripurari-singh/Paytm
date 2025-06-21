2a291802525ff0d0d602257f6904df4e648a5ecb
Tripurari-singh <tripurari5821@gmail.com>
2025-06-21 18:55:34 +0530

    Balance Rendering  Added

--- Frontend/src/components/balance.tsx
+++ Frontend/src/components/balance.tsx
@@ -1,15 +1,24 @@
+import axios from "axios";
+import { useState } from "react"
 
-interface valueInterface {
-    value : number
-} 
-export function Balance({value} : valueInterface){
+
+export function Balance(){
+    const [balance , setBalance] = useState(0);
+    const token = localStorage.getItem("token")
+    axios.get("http://localhost:3000/api/v1/Account/balance" , {
+        headers : {
+            Authorization : `Bearer ${token}`
+        }
+    }).then((response) => {
+        setBalance(response.data.balance)
+    })
     return (
         <div className="flex shadow-slate-400 bg-white shadow-md hover:shadow-2xl transition-all duration-300 m-6 p-6 rounded-2xl border border-gray-200">
             <div className="text-bold text-2xl p-3">
                  Your Balance :
             </div>
-            <div  className="text-bold text-2xl p-3">
-                {value}
+            <div className="text-bold text-2xl p-3">
+                {balance}
             </div>
         </div>
     )
